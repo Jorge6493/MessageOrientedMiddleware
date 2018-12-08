f37c561b5d1cc84bcfa862f8929e542b84a0b73f
Jorge Ramos <jorgeandre006@hotmail.com>
2018-12-08 00:07:19 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,38 @@
+import socketserver
+from collections import deque
+
+class MyUDPHandler(socketserver.BaseRequestHandler):
+    """
+    This class works similar to the TCP handler class, except that
+    self.request consists of a pair of data and client socket, and since
+    there is no connection the client address must be given explicitly
+    when sending data back via sendto().
+    """
+
+    def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+        print("{} wrote:".format(self.client_address[0]))
+        print(data)
+        socket.sendto(data.upper(), self.client_address)
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+    with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
+        server.serve_forever()
+
+class Group():
+    list User
+
+    messages = deque(["Eric", "John", "Michael"])
+
+
+    def addUser
+    def removeUser
+    def sendMessage
+
+
+class Admin():
+    group = Group()
+    def addGroup
+    def deleteGroup

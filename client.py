fdb57e7252c2029c027baf3b1e706d8ced36918e
Jorge Ramos <jorgeandre006@hotmail.com>
2018-12-09 22:42:59 -0400

    threads

--- client.py
+++ client.py
@@ -1,16 +1,52 @@
 import socket
-import sys
+import threading
+import msvcrt
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
+# Define the message to the server
+# bytesToSend = str.encode(msgFromClient)
+# Buffer size for receiving the datagrams from server
+bufferSize = 1024
+# Server IP address and Port number
+serverAddressPort = ("127.0.0.1", 9999)
+# Connect2Server forms the thread - for each connection made to the serve
+#Create a socket instance - A datagram socket
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+def Connect2Server():
+   while True:
 
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      msgFromClient = input()
+      #while not msvcrt.kbhit():
+      # msgFromClient = msvcrt.getch()
+      bytesToSend = str.encode(msgFromClient)
+      # bytesToSend = msgFromClient
+      # Send message to server using created UDP socket
+      UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+      
+      # Receive message from the server
+      msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+      msg = "Message from Server {}".format(msgFromServer[0])
+      print(msg)
 
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
-received = str(sock.recv(1024), "utf-8")
+def recevefromserver():
+   while True:
+      # Receive message from the server
+      msgFromServer = UDPClientSocket.recv(bufferSize)
+      msg = "Message from Server {}".format(msgFromServer[0])
+      print(msg)
 
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+print("Client - Main thread started")  
+# ThreadList  = []
+# ThreadCount = 20
+# Create as many connections as defined by ThreadCount
+# for index in range(ThreadCount):
+# ThreadInstance = threading.Thread(target=Connect2Server())
+# ThreadList.append(ThreadInstance)
+
+threadReceive = threading.Thread(target=recevefromserver())
+# ThreadList.append(threadReceive)
+
+# ThreadInstance.start()
+threadReceive.start()
+# Main thread to wait till all connection threads are complete
+# for index in range(2):
+#     ThreadList[index].join()
--- server.py
+++ server.py
@@ -1,38 +1,61 @@
 import socketserver
-from collections import deque
-
-class MyUDPHandler(socketserver.BaseRequestHandler):
-    """
-    This class works similar to the TCP handler class, except that
-    self.request consists of a pair of data and client socket, and since
-    there is no connection the client address must be given explicitly
-    when sending data back via sendto().
-    """
-
+import threading
+import socket
+# Create a tuple with IP Address and Port Number
+ServerAddress = ("127.0.0.1", 9999)
+# Subclass the DatagramRequestHandler
+client_list = {""}
+client_list.clear()
+# elinput = 
+class MyUDPRequestHandler(socketserver.DatagramRequestHandler):
+    # Override the handle() method
     def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        print("{} wrote:".format(self.client_address[0]))
-        print(data)
-        socket.sendto(data.upper(), self.client_address)
+        # Receive and print the datagram received from client
+        
+        client_list.add(self.client_address)
+        print("Recieved one request from {}:{}".format(self.client_address[0],self.client_address[1]))
+        datagram = self.rfile.readline().strip()
+        print("Datagram Recieved from client is:".format(datagram))
+        print(datagram)  
+        # Print the name of the thread
+        print("Thread Name:{}".format(threading.current_thread().name))
+        # Send a message to the client
+        self.wfile.write(datagram)
+        print("count client_list")
+        print(len(client_list))
+        for x in client_list:
+            print(x)
+        
+        # if elinput == "1":
+        #     self.wfile.write("hihowareya")
+        
+
+        # data = self.request[0].strip()
+        # socket = self.request[1]
+        # print("{}:{} wrote:".format(self.client_address[0],self.client_address[1]))
+        # print(data)
+        # socket.sendto(data.upper(), self.client_address)
+
+# Create a Server Instance
+# UDPServerObject = socketserver.ThreadingUDPServer(ThreadingMixIn, UDPServer)
+def sendmessage():
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    while True:
+       msg = input()
+       for x in client_list:
+           sock.sendto(str.encode(msg),  x)
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
-    with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
-        server.serve_forever()
-
-class Group():
-    list User
-
-    messages = deque(["Eric", "John", "Michael"])
-
-
-    def addUser
-    def removeUser
-    def sendMessage
-
-
-class Admin():
-    group = Group()
-    def addGroup
-    def deleteGroup
+    
+    UDPServerObject = socketserver.ThreadingUDPServer(ServerAddress,MyUDPRequestHandler,True)
+    # UDPServerObject.allow_reuse_address
+    with UDPServerObject:
+        server_thread = threading.Thread(target=UDPServerObject.serve_forever)
+        server_thread.daemon = True
+        server_thread.start()
+        message_thread = threading.Thread(target=sendmessage)
+        message_thread.start()
+        server_thread.join()
+    # Make the server wait forever serving connections
+    # UDPServerObject.serve_forever()
+  
